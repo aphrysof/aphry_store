0fd4b94348619cdd984db9e835e88e45a4aef3a3
aphrysof <sophieabdillahi65@gmail.com>
2024-05-20 09:04:11 +0300

    removed unused decorators

--- src/app/cart-item-card/cart-item-card.component.ts
+++ src/app/cart-item-card/cart-item-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, OnChanges, Output, EventEmitter} from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 import {Product} from "../interfaces/aphry-store";
 import {NgForOf} from "@angular/common";
 import {CartService} from "../services/cart.service";
